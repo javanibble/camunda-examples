ae13bfc1fa36851f2da94698d77ba0110b071a7f
javanibble <javanibble@gmail.com>
2021-02-15 20:09:09 +0100

    Restructure Projects

--- basic/bpmn-event-gateway/run_process_01.sh
+++
@@ -1,4 +0,0 @@
-curl --location --request POST 'http://localhost:8080/rest/message' --header 'Content-Type: application/json' --data-raw '{
-  "messageName" : "secretmessage",
-  "businessKey" : "myfirstgame"
-}'
