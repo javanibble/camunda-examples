fef5e79130f573ef5249da70e4a7190b0dc0aaea
javanibble <javanibble@gmail.com>
2021-02-15 19:54:14 +0100

    Removed old projects

--- basic/bpmn-inclusive-gateway/run_process_01.sh
+++
@@ -1,8 +0,0 @@
-curl --location --request POST 'http://localhost:8080/rest/process-definition/key/battleplan/start' --header 'Content-Type: application/json' --data-raw '{
-     "variables": {
-         "user": {
-             "value": "Andre",
-             "type": "String"
-        }
-    }
-}'
