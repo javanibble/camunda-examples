ec38fcfbc5a1c7e88185b51517989a469a21ca7d
javanibble <javanibble@gmail.com>
2021-02-19 08:08:23 +0100

    Add BPMN Timer Event: Basic Example

---
+++ basic/event-timer/src/main/java/com/javanibble/camunda/examples/BasicTimerEventApplication.java
@@ -0,0 +1,13 @@
+package com.javanibble.camunda.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTimerEventApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BasicTimerEventApplication.class);
+  }
+
+}
