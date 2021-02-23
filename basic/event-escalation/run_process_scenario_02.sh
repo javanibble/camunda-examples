565f8dc7556839668381ad136ebf6debdea6a177
javanibble <javanibble@gmail.com>
2021-02-23 09:03:31 +0100

    Add additional scripts.

---
+++ basic/event-escalation/run_process_scenario_02.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo Escalation Event Process 1: Complete User Task 1
+IP=$(curl --location --fail --silent --request POST 'http://localhost:8080/rest/task' --header 'Content-Type: application/json' --data-raw '{ "processInstanceBusinessKey": "process-key-123", "taskDefinitionKey": "user-task-1"}' | jq -r '.[0].id')
+
+curl --location --fail --silent --request POST "http://localhost:8080/rest/task/$IP/complete" --header 'Content-Type: application/json'
+
