86fa458e9b45b06adf3b6154d7bb73b322934796
javanibble <javanibble@gmail.com>
2021-03-04 10:25:47 +0100

    Update BPMN and Java Delegate

--- basic/gateway-exclusive/src/main/java/com/javanibble/camunda/examples/ConsoleLoggerDelegate.java
+++ basic/gateway-exclusive/src/main/java/com/javanibble/camunda/examples/ConsoleLoggerDelegate.java
@@ -12,26 +12,7 @@ public class ConsoleLoggerDelegate implements JavaDelegate {
     private final Logger LOGGER = LoggerFactory.getLogger(ConsoleLoggerDelegate.class.getName());
 
     public void execute(DelegateExecution execution) throws Exception {
-        String activityId = execution.getCurrentActivityId();
-
-        if ("retrieve-coffee-order".equals(activityId)) {
-            LOGGER.info("Order Coffee Process: Retrieve Coffee Order");
-
-        } else if ("make-espresso".equals(activityId)) {
-            LOGGER.info("Order Coffee Process: Make Espresso");
-
-        } else if ("make-caffe-mocha".equals(activityId)) {
-            LOGGER.info("Order Coffee Process: Make Caffe Mocha");
-
-        } else if ("give-free-sample".equals(activityId)) {
-                LOGGER.info("Order Coffee Process: Give Free Sample");
-
-        } else if ("deliver-coffee-order".equals(activityId)) {
-            LOGGER.info("Order Coffee Process: Deliver Coffee Order");
-
-        } else {
-            LOGGER.info("Something went wrong !!!!");
-        }
+        LOGGER.info("Order Coffee Process: " + execution.getCurrentActivityName());
     }
 
 }
