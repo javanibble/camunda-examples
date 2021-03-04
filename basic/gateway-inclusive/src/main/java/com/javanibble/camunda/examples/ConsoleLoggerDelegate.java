52fcfd4c83b7a59a62c95ad20e625dede5e6b2d1
javanibble <javanibble@gmail.com>
2021-03-04 16:23:00 +0100

    Update Delegate class by simplifying the logging.

--- basic/gateway-inclusive/src/main/java/com/javanibble/camunda/examples/ConsoleLoggerDelegate.java
+++ basic/gateway-inclusive/src/main/java/com/javanibble/camunda/examples/ConsoleLoggerDelegate.java
@@ -12,26 +12,7 @@ public class ConsoleLoggerDelegate implements JavaDelegate {
     private final Logger LOGGER = LoggerFactory.getLogger(ConsoleLoggerDelegate.class.getName());
 
     public void execute(DelegateExecution execution) throws Exception {
-        String activityId = execution.getCurrentActivityId();
-
-        if ("place-order".equals(activityId)) {
-            LOGGER.info("Place Order Process: Place Order");
-
-        } else if ("make-coffee".equals(activityId)) {
-            LOGGER.info("Place Order Process: Make Coffee");
-
-        } else if ("warm-up-muffin".equals(activityId)) {
-            LOGGER.info("Place Order Process: Warm Up Muffin");
-
-        } else if ("give-free-sample".equals(activityId)) {
-                LOGGER.info("Place Order Process: Give Free Sample");
-
-        } else if ("deliver-order".equals(activityId)) {
-            LOGGER.info("Place Order Process: Deliver Order");
-
-        } else {
-            LOGGER.info("Something went wrong !!!!");
-        }
+        LOGGER.info("Place Order Process: " + execution.getCurrentActivityName());
     }
 
 }
