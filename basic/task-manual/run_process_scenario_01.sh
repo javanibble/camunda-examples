918762da8bc0e199c5c11dfe6471e29705709a5e
javanibble <javanibble@gmail.com>
2021-04-05 10:00:34 +0200

    Update Script

--- basic/task-manual/run_process_scenario_01.sh
+++ basic/task-manual/run_process_scenario_01.sh
@@ -1,9 +1,2 @@
-curl --location --request POST 'http://localhost:8080/rest/process-definition/key/order-coffee/start' --header 'Content-Type: application/json' --data-raw '{
-     "variables": {
-         "order": {
-             "value": "Espresso",
-             "type": "String"
-        }
-    }
-}'
+curl --location --request POST 'http://localhost:8080/rest/process-definition/key/order-coffee/start' --header 'Content-Type: application/json'
 
